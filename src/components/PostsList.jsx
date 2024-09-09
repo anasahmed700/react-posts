f4c6d7c9b8b44a84e5e24dc369b869ffdf17d0aa
Anas Ahmed <anasahmed700@gmail.com>
2024-09-09 17:28:25 +0500

    lifting state up

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import NewPost from './NewPost'
 import Post from './Post'
 import classes from './PostsList.module.css'
 function PostsList() {
+    const [enteredBody, setEnteredBody] = useState('');
+    const [enteredAuthor, setEnteredAuthor] = useState('');
+
+    function bodyChangeHandler(event) {
+        setEnteredBody(event.target.value)
+    }
+    function authorChangeHandler(event) {
+        setEnteredAuthor(event.target.value)
+    }
+
     return (
         <>
-            <NewPost />
+            <NewPost onBodyChange={bodyChangeHandler} onAuthorChange={authorChangeHandler} />
             <ul className={classes.posts}>
-                <Post author="Anas" body="Keep coding" />
+                <Post author={enteredAuthor} body={enteredBody} />
                 <Post author="Ahmed" body="Learn react.js" />
             </ul>
         </>
