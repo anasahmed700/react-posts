feede994c33bb0bf9dd6f3de0c5cd62a101a2a2f
Anas Ahmed <anasahmed700@gmail.com>
2024-09-13 15:24:27 +0500

    setup backend & sending post request

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -7,6 +7,15 @@ import { useState } from 'react';
 function PostsList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([]);
     function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts',
+            {
+                method: 'POST',
+                body: JSON.stringify(postData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        )
         setPosts((existingPosts) => [postData, ...existingPosts])
     }
     return (
@@ -23,7 +32,7 @@ function PostsList({ isPosting, onStopPosting }) {
                     {posts.map(post => <Post key={post.body} author={post.author} body={post.body} />)}
                 </ul>
             }{posts.length <= 0 &&
-                <div style={{textAlign: 'center', color: 'white'}}>
+                <div style={{ textAlign: 'center', color: 'white' }}>
                     <h2>There are no posts yet.</h2>
                     <p>Start adding some!</p>
                 </div>
